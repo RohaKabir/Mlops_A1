da836c3c748d380a7355c8386cf9f40149649fe6
RohaKabir <rohakabir92@gmail.com>
2024-03-29 23:53:12 +0500

    update 11

--- code/code/model.py
+++ code/code/model.py
@@ -4,26 +4,34 @@ from sklearn.model_selection import train_test_split
 from sklearn.neighbors import KNeighborsClassifier
 from sklearn.metrics import accuracy_score
 
+
 # Load the Iris dataset from library
 iris = load_iris()
 X = iris.data
 y = iris.target
+
 
 # Split the dataset into training and testing sets
 X_train, X_test, y_train, y_test = train_test_split(
     X, y, test_size=0.2, random_state=42
 )
 
+
 # Initialize the KNN classifier
 knn = KNeighborsClassifier(n_neighbors=3)
+
 
 # Train the classifier
 knn.fit(X_train, y_train)
 
+
 # Make predictions on the testing set
 predictions = knn.predict(X_test)
 
+
 # Calculate accuracy
 accuracy = accuracy_score(y_test, predictions)
+
+
 # Printing the accuracy of the trained model
 print("Accuracy of the trained model is:", accuracy)
